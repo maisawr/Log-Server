5a64227975c6ed831288afc4a7f8cd6d886727a6
Maísa Wolff Resplande <maisawres@gmail.com>
2023-02-18 15:12:37 -0500

    Open and write to file.

--- loggingService/loggingService.py
+++ loggingService/loggingService.py
@@ -1,8 +1,19 @@
 import sys
 
-# Open file
-fhand = open("daffodils.txt")
-
 # TO DO
 # file's name can't be hardcoded
 # include in a config file
+# prompt user to write the file's name?
+# argument line command?
+
+fileName = input("Enter the file's name: ")
+
+# Open file with a+ access
+# If the file doesn't exist, it will create a new file and append to it
+# Else, it will append to the existing file
+fhand = open(fileName, 'a+')
+fhand.write("Write log message")
+fhand.close()
+
+# TO DO
+# Refactor code using with open()
