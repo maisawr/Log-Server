9aed47fbb28f959171fb7f6f04fe2c4346e71143
Maísa Wolff Resplande <maisawres@gmail.com>
2023-02-19 12:52:16 -0500

    Get file's name from command line.

--- loggingService/loggingService.py
+++ loggingService/loggingService.py
@@ -1,12 +1,29 @@
 import sys
 
-# TO DO
+# REQUIREMENTS
 # file's name can't be hardcoded
 # include in a config file
-# prompt user to write the file's name?
 # argument line command?
+# logs must be saved in a plain text file
+# how to format the message?
+# how to identify levels?
+# how to connect to other device (TCP/UDP)?
+# how transform into a service?
 
-fileName = input("Enter the file's name: ")
+
+# Get arguments from command line
+userInput = sys.argv
+
+# Parse command line arguments starting from second argument
+# The first argument is the program's name
+for command in range(1, len(userInput)):
+    fileName = sys.argv[1]
+    
+# If no argument is passed, use default file
+    # Call function to open and write to the file
+# Else, check if argument is valid .txt file
+    # If it's a valid file name, call function to open and write to the file
+    # Else, show usage message and quit program
 
 # Open file with a+ access
 # If the file doesn't exist, it will create a new file and append to it
@@ -17,3 +34,5 @@ fhand.close()
 
 # TO DO
 # Refactor code using with open()
+# Refactor code to transform open/write file into a function
+
