4951ea4bb5761bc346d95e4fd21e4a5dedd3f752
Maísa Wolff Resplande <maisawres@gmail.com>
2023-02-24 14:35:39 -0500

    Add function for log in JSON format as a string.

--- loggingService/writeLog.py
+++ loggingService/writeLog.py
@@ -1,7 +1,8 @@
 from datetime import datetime
+import json
 
 # Constants definitions
-json_format = "json"
+json_format = "JSON"
 
 def logger(file_name, request_data, log_format):
         # Open file with a+ access
@@ -11,7 +12,7 @@ def logger(file_name, request_data, log_format):
         fhand = open(file_name, 'a+')
 
         if (log_format == json_format):
-            print("json")
+            formatted_log = format_as_json(request_data)
         else:
             formatted_log = format_as_syslog(request_data)
 
@@ -44,29 +45,53 @@ def format_as_syslog(request_data):
         return formatted_log
 
 
+
+# JSON format
+# Format the log as a JSON string
+def format_as_json(request_data):
+        
+        # Format timestamp
+        date_time_str = get_timestamp().strftime("%d %m %Y %H:%M:%S")
+
+        # Parse request message
+        request_data = request_data.split(":")
+        client_name = request_data[0].strip()
+        process_name = request_data[1].strip()
+        log_level = request_data[2].strip()
+        process_id = request_data[3].strip() 
+        log_message = request_data[4].strip()
+        
+        # Log formatted as JSON
+        # Create a dictionary
+        formatted_log = {
+            "timestamp": date_time_str,
+            "message": log_message,
+            "log": {
+                "level": log_level,
+                "file": process_name,
+                "line": "",
+            },
+            "user": {
+                "name": client_name,
+                "id": "",
+            },
+            "event": {
+                "success": True
+            }
+        }
+
+        # Convert dictionary to JSON format
+        JSON_log = json.dumps(formatted_log)
+
+        formatted_log = JSON_log
+
+        return formatted_log
+
+
+
 # Get timestamp for current date and time
 def get_timestamp():
         current_time = datetime.now()
         time_stamp = current_time.timestamp()
         date_time = datetime.fromtimestamp(time_stamp)
         return date_time
-
-
-
-# JSON format
-#{
-#    "timestamp": "",
-#    "message": "",
-#    "log": {
-#        "level": "",
-#        "file": "",
-#        "line": 1,
-#    },
-#    "user": {
-#        "name": "",
-#        "id": "",
-#    },
-#    "event": {
-#        "success": True
-#    }
-#}
